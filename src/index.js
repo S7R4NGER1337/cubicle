c91d52fc5be94fe1056360a27211ad360a778447
S7R4NGER3301 <nikolakoew@gmail.com>
2023-06-03 13:53:47 +0300

    add home controller

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-const express = require('express')
-const handlebars = require('express-handlebars')
-const path = require('path')
-const app = express()
-const expressConfig = require('./config/expressConf')
-const handlebarsConfig = require('./config/hanfledarsConfig')
+const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const app = express();
+const expressConfig = require("./config/expressConf");
+const handlebarsConfig = require("./config/hanfledarsConfig");
+const homeController = require("./controllers/homeController");
 
-expressConfig(app)
-handlebarsConfig(app)
+expressConfig(app);
+handlebarsConfig(app);
 
+app.use(homeController);
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
-
-
-app.listen(1337)
+app.listen(1337);
