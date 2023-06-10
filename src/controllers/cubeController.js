f1aaddab404db686ae883a25729c3beb55c26b7c
S7R4NGER3301 <nikolakoew@gmail.com>
2023-06-10 14:32:39 +0300

    adding cube details

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -10,9 +10,20 @@ router.get("/create", (req, res) => {
 router.post("/create", (req, res) => {
   const { name, description, imageUrl, difficultyLevel } = req.body;
 
-  cubeManager.create({ name, description, imageUrl, difficultyLeve: Number(difficultyLevel) });
+  cubeManager.create({
+    name,
+    description,
+    imageUrl,
+    difficultyLeve: Number(difficultyLevel),
+  });
 
   res.redirect("/");
+});
+
+router.get("/:cubeId/details", (req, res) => {
+
+  const cube = cubeManager.getOne(req.params.cubeId);
+  res.render("details", { cube });
 });
 
 module.exports = router;
--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,7 +1,24 @@
 const uniqid = require('uniqid')
-const db = [];
+const db = [
+  {
+    id: 'hg8igt8lipwlvdg',
+    name: 'Classic rubic cube',
+    description: 'Cool cube',
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyuFX_QLa5u3t0fn2S3bSlTqX-lSLvvbsZBw&usqp=CAU",
+    difficultyLevel: 4,
+  },
+  {
+    id: 'qtwncteh562kane',
+    name: 'Pyramid',
+    description: 'Cool pyramid',
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuIvpMVAWW5KnJX-kmM-qlIrFBUk5irC3G3TMt4HXTC1CUtBA4DUcTgucn9QPzS-1PRKI&usqp=CAU",
+    difficultyLevel: 3,
+  },
+];
 
 exports.getAll = () => db.slice();
+
+exports.getOne = (cubeId) => db.find(x => x.id == cubeId)
 
 exports.create = (cubeData) => {
   const newCube = {
