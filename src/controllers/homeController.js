020d6b1a2feecd4459b83367a44183fa123f9592
S7R4NGER3301 <nikolakoew@gmail.com>
2023-06-10 14:13:12 +0300

    Showing cubes in home

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const cubeManager = require("../managers/cubeManager");
 
 router.get("/", (req, res) => {
-  res.render("index");
+  const cubes = cubeManager.getAll();
+  res.render("index", { cubes });
 });
 
-router.get('/about', (req, res)=> {
-    res.render('about')
-})
+router.get("/about", (req, res) => {
+  res.render("about");
+});
 
-module.exports = router
+module.exports = router;
