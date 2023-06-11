5a0fcd7ed6f19103df75de9a98da51982bcd16f0
S7R4NGER3301 <nikolakoew@gmail.com>
2023-06-11 16:24:19 +0300

    setup mongoose

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const expressConfig = require("./config/expressConf");
 const handlebarsConfig = require("./config/hanfledarsConfig");
-const routes = require('./routes')
+const routes = require("./routes");
+const dbConnect = require("./config/dbConfig");
+
+dbConnect()
+  .then(() => console.log("Db connected"))
+  .catch((err) => {
+    console.log("Db err:", err);
+  });
 
 expressConfig(app);
 handlebarsConfig(app);
 
-app.use(routes)
-
+app.use(routes);
 app.listen(1337);
